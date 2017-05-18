8e5afce6d1108dd8f11b810df8624f9705c7041a
Dustin Blackman <dev@dustinblackman.com>
2017-05-18 16:35:39 -0400

    launcher secondary

--- src/public/lollauncher.js
+++ src/public/lollauncher.js
@@ -1,5 +1,5 @@
 window.genVideoHTML = function genVideoHTML(nick) {
   var video_class = 'video';
-  // if (nick.toLowerCase() === 'dustin') video_class = 'secondary';
+  if (nick.toLowerCase() === 'dustin') video_class = 'secondary-video';
   return `<div id="vid_${nick}" class="${video_class} animated slideInLeft"><img src="/img/client.png" /><span>${nick}</span></div>`;
 };
--- src/public/stream.js
+++ src/public/stream.js
@@ -1,3 +1,4 @@
+let users = [];
 const webrtc = new SimpleWebRTC({ // eslint-disable-line
   localVideoEl: '',
   remoteVideosEl: '',
@@ -9,15 +10,15 @@ const webrtc = new SimpleWebRTC({ // eslint-disable-line
 
 const addVideo = (video, peer) => {
   const html = window.genVideoHTML(peer.nick);
-  let users = $('.video > span').map((idx, el) => {
-    return $(el).text();
-  }).get();
 
   users.push(peer.nick);
   users.sort();
   const user_idx = users.findIndex(entry => entry === peer.nick);
 
-  if (user_idx === 0) {
+  // Lazy, make it hacky for now
+  if (window.location.href.includes('lollauncher') && (peer.nick.toLowerCase() === 'dustin')) {
+    $('#secondary').append(html);
+  } else if (user_idx === 0) {
     $('#videos').prepend(html);
   } else if (user_idx === (users.length - 1)) {
     $('#videos').append(html);
@@ -32,6 +33,7 @@ webrtc.on('videoAdded', addVideo);
 
 webrtc.on('videoRemoved', (video, peer) => {
   const el = $(`#vid_${peer.nick}`);
+  users = users.splice(users.indexOf(peer.nick), 1);
   el.addClass('slideOutLeft');
   el.on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
     el.remove();
