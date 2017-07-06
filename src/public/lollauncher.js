079b8198c076af19810a01928dc794aedd9712a4
Dustin Blackman <dev@dustinblackman.com>
2017-07-06 18:29:46 -0400

    fix omer

--- src/public/lollauncher.js
+++ src/public/lollauncher.js
@@ -1,6 +1,6 @@
 window.genVideoHTML = function genVideoHTML(nick) {
   var video_class = 'video';
   if (window.location.href.includes('omer-lollauncher') && nick.toLowerCase() === 'omer') video_class = 'firstly-video';
-  if (nick.toLowerCase() === 'dustin') video_class = 'secondary-video';
+  if (nick.toLowerCase() === 'dustin' || (!window.location.href.includes('omer-lollauncher') && nick.toLowerCase() === 'omer')) video_class = 'secondary-video';
   return `<div id="vid_${nick}" class="${video_class} animated slideInLeft"><img src="/img/client.png" /><span>${nick}</span></div>`;
 };
